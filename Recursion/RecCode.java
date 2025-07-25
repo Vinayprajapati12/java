02fae8274389ebbb6db761be003cf36f37fc0893
Vinayprajapati12 <vinayprajapati120103@gmail.com>
2025-07-25 19:56:12 +0530

    Update RecCode.java

--- Recursion/RecCode.java
+++ Recursion/RecCode.java
@@ -1,1 +1,22 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class RecQue {
+    public static void main(String[] args) {
+        System.out.println("Enter any no: ");
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        OneToN(n);
+    }
+
+    // 1 to N numbers
+
+    static void OneToN(int n){
+        if(n==0)
+            return;
+        OneToN(n-1);
+        System.out.println(n);
+    }
+}
 
