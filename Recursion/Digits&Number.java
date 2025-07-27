b27f7c2044a784dd990a2c1878c5507f05392576
Vinayprajapati12 <vinayprajapati120103@gmail.com>
2025-07-27 21:42:11 +0530

    Update Digits&Number.java

--- Recursion/Digits&Number.java
+++ Recursion/Digits&Number.java
@@ -3,6 +3,9 @@ package recursion;
 import java.util.Scanner;
 
 public class digitsAndNumbers {
+
+    // static int reverse = 0; -> Reverse Number
+    
     public static void main(String[] args) {
         System.out.println("Enter any no: ");
         Scanner sc = new Scanner(System.in);
@@ -16,6 +19,9 @@ public class digitsAndNumbers {
 
         // 3.int product = productOfDigits(n);
         // System.out.println("Product of digits: " + product);
+
+        // 4. reverseNumber(n);
+        // System.out.println("Reversed number: " + reverse);
     }
 
     // Count number of digits in a number
@@ -39,6 +45,14 @@ public class digitsAndNumbers {
         if (n == 0) return 0;       // Edge case: if the number is 0, return 0
         if (n < 10) return n;       // Base case: single digit
         return (n % 10) * productOfDigits(n / 10);
+    }
+
+    // Reverse a number
+
+    static void reverseNumber(int n) {
+        if (n == 0) return;
+        reverse = reverse * 10 + n % 10;
+        reverseNumber(n / 10);
     }
 }
 
