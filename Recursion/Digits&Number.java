5ee6cc6695081a4f6f7ccc9b4c2665a355658255
Vinayprajapati12 <vinayprajapati120103@gmail.com>
2025-07-27 12:02:39 +0530

    Update Digits&Number.java

--- Recursion/Digits&Number.java
+++ Recursion/Digits&Number.java
@@ -1,1 +1,24 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class digitsAndNumbers {
+    public static void main(String[] args) {
+        System.out.println("Enter any no: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // 1.int count = countDigits(n);
+        //System.out.println("Number of digits: " + count);
+    }
+
+    // Count number of digits in a number
+
+    static int countDigits(int n) {
+        if (n == 0) return 0;
+        return 1 + countDigits(n / 10);
+    }
+
+
+}
 
