8975ed53a05b975b6e6e79d16973282e7bcf2377
Vinayprajapati12 <vinayprajapati120103@gmail.com>
2025-07-30 19:39:17 +0530

    Update StringRecursion.java

--- Recursion/StringRecursion.java
+++ Recursion/StringRecursion.java
@@ -1,1 +1,26 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class stringRecursion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any string: ");
+        String str = sc.nextLine();
+
+        // 1. printChars(str, 0);
+        
+    }
+
+
+    // Print each character of a string 
+    
+    static void printChars(String str, int index) {
+        if (index == str.length()) {
+            return;
+        }
+        System.out.println(str.charAt(index));
+        printChars(str, index + 1);
+    }
+}
 
