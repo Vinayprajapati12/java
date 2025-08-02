c6f82b3ac3b9d1df8d99281c43c2ccfaf9ba3c20
Vinayprajapati12 <vinayprajapati120103@gmail.com>
2025-08-02 12:41:19 +0530

    Update ArrayRecursion.java

--- Recursion/ArrayRecursion.java
+++ Recursion/ArrayRecursion.java
@@ -2,12 +2,16 @@ package recursion;
 
 public class arrayRecursion {
     public static void main(String[] args) {
-        int[] arr = {10, 20, 30, 40, 50};
-        printArray(arr, 0);
+        /*int[] arr = {10, 20, 30, 40, 50};
+        printArray(arr, 0);*/
+
+        /*int[] arr = {5, 10, 15};
+        int sum = findSum(arr, 0);
+        System.out.println("Sum = " + sum);*/
     }
 
 
-    // Print elements of an array
+    // 1. Print elements of an array
     
     static void printArray(int[] arr, int index) {
         // Base condition
@@ -20,5 +24,16 @@ public class arrayRecursion {
 
         // Recursive call for next index
         printArray(arr, index + 1);
+    }
+
+    // 2. Find sum of all elements in an array
+
+    static int findSum(int[] arr, int index) {
+        // Base case
+        if (index == arr.length) {
+            return 0;
+        }
+        // Recursive case
+        return arr[index] + findSum(arr, index + 1);
     }
 }
