c40ae5ba2a7b62b2af46f537da4a274ad0f53fb0
Vinayprajapati12 <vinayprajapati120103@gmail.com>
2025-08-02 13:54:24 +0530

    Update Logical&Pattern.java

--- Recursion/Logical&Pattern.java
+++ Recursion/Logical&Pattern.java
@@ -1,1 +1,27 @@
+package recursion;
 
+import java.util.Scanner;
+
+public class logicalAndPattern {
+    public static void main(String[] args) {
+       /* Scanner sc = new Scanner(System.in);
+        System.out.print("Enter base (x): ");
+        int x = sc.nextInt();
+
+        System.out.print("Enter exponent (n): ");
+        int n = sc.nextInt();
+
+        int result = power(x, n);
+        System.out.println(x + "^" + n + " = " + result);*/
+    }
+
+
+    // 1. Calculate x^n using recursion (power function)
+
+    static int power(int x, int n) {
+        if (n == 0) {
+            return 1; // base case: x^0 = 1
+        }
+        return x * power(x, n - 1); // recursive step
+    }
+}
